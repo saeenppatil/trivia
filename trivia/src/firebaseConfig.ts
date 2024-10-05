63e7fa7e2d89ba1f0c265358146c18fa5d9cf5cf
Saee Patil <saee@campus-010-059.ucdavis.edu>
2024-10-04 17:59:28 -0700

    env stuff

--- trivia/src/firebaseConfig.ts
+++ trivia/src/firebaseConfig.ts
@@ -5,13 +5,13 @@ import { getAnalytics } from 'firebase/analytics';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDxFwTXhd2qzvshaHhdC1TrCSW2BGl8tgk",
-  authDomain: "trivia-f7176.firebaseapp.com",
-  projectId: "trivia-f7176",
-  storageBucket: "trivia-f7176.appspot.com",
-  messagingSenderId: "897896598617",
-  appId: "1:897896598617:web:7d9924ecd4066a773cf548",
-  measurementId: "G-0MFF7NJL9C"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
