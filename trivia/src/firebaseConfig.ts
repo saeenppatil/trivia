d3163a26525fa4d42cd1b6ebd50d9a6e3c260f0e
saeenppatil <118249396+saeenppatil@users.noreply.github.com>
2025-02-17 22:54:02 -0800

    Update firebaseConfig.ts

--- trivia/src/firebaseConfig.ts
+++ trivia/src/firebaseConfig.ts
@@ -1,9 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getAnalytics } from 'firebase/analytics';
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -18,5 +17,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-// Initialize Firebase Authentication and export it
+// Initialize Firebase Authentication 
 export const auth = getAuth(app);
